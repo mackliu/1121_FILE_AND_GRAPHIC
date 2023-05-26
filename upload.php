eef62a3c014c74b366118f4dd8092cab5f91cd1f
kai <cgk0910@gmail.com>
2023-05-26 14:32:45 +0800

    將檔案資訊寫入資料表,並可透過PHP在網頁上呈現上傳的檔案

--- upload.php
+++ upload.php
@@ -15,6 +15,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案上傳</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        img{
+            width:200px;
+            border:10px solid black;
+            margin:3px;
+        }
+    </style>
 </head>
 <body>
  <h1 class="header">檔案上傳練習</h1>
@@ -37,6 +44,17 @@
 
 <!----建立一個連結來查看上傳後的圖檔---->  
 
+<?php 
+$dsn="mysql:host=localhost;charset=utf8;dbname=upload";
+$pdo=new PDO($dsn,'root','');
+
+$sql="select * from `images` ";
+$imgs=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($imgs as $img){
+    echo "<img src='./img/{$img['img']}'>";
+}
+?>
 
 </body>
 </html>
