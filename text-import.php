8009c4958db3dde0582cbecee9968a8b63d26add
macklium <macklun@gmail.com>
2023-08-24 15:44:57 +0800

    讀入CSV檔並將內容印出在頁面上

--- text-import.php
+++ text-import.php
@@ -9,6 +9,31 @@
  * 7.結束檔案
  */
 
+
+if(!empty($_FILES['text']['tmp_name'])){
+    echo "檔名是:".$_FILES['text']['name'];
+    echo "<br>";
+    echo "檔案大小是:".$_FILES['text']['size'];
+    echo "<hr>";
+
+    move_uploaded_file($_FILES['text']['tmp_name'],"./document/{$_FILES['text']['name']}");
+    $path="./document/{$_FILES['text']['name']}";
+    // 使用 'r' 模式來讀取檔案
+$file = fopen($path, "r");
+
+if ($file) {
+    // 讀取檔案到最後
+    while (($line = fgets($file)) !== false) {
+        echo $line;
+        echo "<br>";
+    }
+    fclose($file);
+} else {
+    // 檔案開啟失敗
+    echo "檔案開啟失敗";
+}
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,7 +47,11 @@
 <body>
 <h1 class="header">文字檔案匯入練習</h1>
 <!---建立檔案上傳機制--->
+<form action="?" method="post" enctype="multipart/form-data">
+<input type="file" name="text" id="text">
+<input type="submit" value="上傳">
 
+</form>
 
 
 <!----讀出匯入完成的資料----->
